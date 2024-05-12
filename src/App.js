78e125b16f0d3d29cf4df7a519d52afdacbe97a7
Zafarzhon <shakirowww@inbox.ru>
2024-05-13 01:59:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import Example from "./components/Example/Example";
+
+import Header from "./components/header/Header";
+import Section from "./components/main/Section";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Section />
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      {/* <Example /> */}
+    </div>
+  );
+}
+
+export default App;
